442b870772c62235b58bc6fd97314811ad7b9da8
sasakisenhi <33shin12@gmail.com>
2026-04-19 14:07:11 +0900

    Initial commit

---
+++ test/bevy-platform-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BevyPlatformInfra from '../lib/bevy-platform-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bevy-platform-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BevyPlatformInfra.BevyPlatformInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
