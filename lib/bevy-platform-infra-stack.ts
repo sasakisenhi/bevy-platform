bead4336c64376c9fb7dc4d315671251052f4562
sasakisenhi <33shin12@gmail.com>
2026-05-02 19:27:21 +0900

    マジックナンバー、ハードコードの削除

--- lib/bevy-platform-infra-stack.ts
+++ lib/bevy-platform-infra-stack.ts
@@ -1,16 +1,44 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+// 1. 設定値をオブジェクトにまとめる（マジックナンバーの排除）
+const STORAGE_CONFIG = {
+  RETENTION_DAYS: 30,
+  HISTORY_RETENTION_DAYS: 7,
+  BUCKET_PREFIX: 'bevy-artifacts',
+} as const;
 
 export class BevyPlatformInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 実行時に -c env=prod と渡せる
+    const envName = this.node.tryGetContext('env') || 'dev';
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BevyPlatformInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const artifactBucket = new s3.Bucket(this, 'BevyArtifactBucket', {
+      // 環境名とアカウントIDを組み合わせて一意性を担保
+      bucketName: `${STORAGE_CONFIG.BUCKET_PREFIX}-${envName}-${this.account}`,
+      
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+
+      lifecycleRules: [
+        {
+          id: 'ExpireOldBuilds',
+          enabled: true,
+          // 定数を使用
+          expiration: cdk.Duration.days(STORAGE_CONFIG.RETENTION_DAYS),
+          noncurrentVersionExpiration: cdk.Duration.days(STORAGE_CONFIG.HISTORY_RETENTION_DAYS),
+        }
+      ],
+    });
+
+    new cdk.CfnOutput(this, 'BucketNameExport', {
+      value: artifactBucket.bucketName,
+    });
   }
 }
